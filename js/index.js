63aacecd21ba9b9a7ff9c03b60e0900a5f89e3dc
Estrella-BF <gracielabenitesf@gmail.com>
2021-05-17 01:06:49 -0500

    STYLE: company

--- js/index.js
+++ js/index.js
@@ -2,25 +2,36 @@ const buttonQuotation = document.getElementById('btn-quotation');
 const formQuotation = document.getElementById('form-quotation');
 const quotationContainer = document.getElementById('quotation');
 const main = document.getElementById('main');
-const mainPosition = main.offsetTop;
 const navMenuMobile = document.getElementById('nav-menu-mobile');
 const navMenuMobileText = document.getElementById('nav-text-mobile');
-const positionQuotationContainer = quotationContainer.offsetTop;
 
-function quotation() {
-  buttonQuotation.classList.toggle("hidden");
-  formQuotation.classList.toggle("show");
+/*
+Funcionalidad para el boton de cotización ubicado en el header
+*/
+if (quotationContainer) {
+  const positionQuotationContainer = quotationContainer.offsetTop;
+
+  function quotation() {
+    buttonQuotation.classList.toggle("hidden");
+    formQuotation.classList.toggle("show");
+  }
+  
+  function openQuotationForm() {
+    window.scrollTo(0, positionQuotationContainer);
+    quotation();
+  }
 }
 
-function openQuotationForm() {
-  window.scrollTo(0, positionQuotationContainer);
-  quotation();
-}
+
 
   /*
   Verificará la posición del scroll del navegador, y pasará como
   parámetro el scroll del elemento
+  Funcionalidad para cuando el scroll baje el menu se quede fijo
   */
+
+if (main) {
+  const mainPosition = main.offsetTop;
   const setClassScrollPosition = function (scrollPosition) {
     if (scrollPosition > mainPosition) {
       navMenuMobile.classList.add("nav-fixed");
@@ -30,14 +41,13 @@ function openQuotationForm() {
       navMenuMobileText.classList.remove("hidden");
     }
   }
-
   /*
   Cuando inicializa, se verifica la posición del scroll en el navegador
   */
   const windowScrollCurrentPosition  = window.pageYOffset || document.documentElement.scrollTop;
   setClassScrollPosition(windowScrollCurrentPosition);
   
-
+  
   /*
   Cuando se hace scroll al naveador, se verifica la posición del scroll en el navegador
   */
@@ -45,3 +55,5 @@ function openQuotationForm() {
     let scrollPosition = this.scrollY;
     setClassScrollPosition(scrollPosition);
   });
+}
+  
