8d35170849f210320a7b5e7f6b9a539d7dbd8560
Estrella-BF <gracielabenitesf@gmail.com>
2021-05-17 23:20:40 -0500

    FEAT: adding test

--- js/index.js
+++ js/index.js
@@ -22,6 +22,21 @@ if (quotationContainer) {
   }
 }
 
+//test
+function sendMail() {
+  Email.send({
+    Host : "smtp.gmail.com",
+    Username : "gracielabenitesf@gmail.com",
+    Password : "$Egbf1307g$",
+    To : 'estrellabenitesf@gmail.com',
+    From : 'gracielabenitesf@gmail.com',
+    Subject : "This is the subject",
+    Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
+
 
 
   /*
