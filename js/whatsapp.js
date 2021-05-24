260bb3377512f9f3617cb4a9163c64764dfc4bf7
Estrella-BF <gracielabenitesf@gmail.com>
2021-05-24 15:06:44 -0500

    FEAT: cambiando imagenes

--- js/whatsapp.js
+++ js/whatsapp.js
@@ -3,7 +3,7 @@ $('#WAButton').floatingWhatsApp({
   headerTitle: 'Conversa con nosotros!', //Popup Title
   popupMessage: 'Cuál es tu duda?', //Popup Message
   showPopup: true, //Enables popup display
-  buttonImage: '<img src="assets/whatsapp.png" />', //Button Image
+  buttonImage: '<img src="assets/icons/whatsapp.png" />', //Button Image
   //headerColor: 'crimson', //Custom header color
   //backgroundColor: 'crimson', //Custom background button color
   position: "right" //Position: left | right
